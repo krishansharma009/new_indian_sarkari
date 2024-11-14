18d71179b193d95bab415dbee4d3c14d57f4dd5e
krishansharma009 <godkrihna.apple.mtt@gmail.com>
2024-11-14 12:33:27 +0530

    added index.html

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ app.use((req, res, next) => {
 // Route: Home
 app.get("/", (req, res) => {
   logger.debug("Visited home page");
-  res.send("Hello World!");
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 // Error handling middleware (Combined)
