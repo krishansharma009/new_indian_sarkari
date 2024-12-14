331c3338a0edf070e2bb87a6e21257019c771dfa
krishansharma009 <godkrihna.apple.mtt@gmail.com>
2024-12-14 15:19:05 +0530

    get to university

--- api/admissionUpdate/updateAdmissionRout.js
+++ api/admissionUpdate/updateAdmissionRout.js
@@ -12,7 +12,7 @@ router.delete("/:id", AdmissionUpdateController.deleteAdmissionUpdate);
 // New routes for specific sections
 router.get("/get/latest-admision", AdmissionUpdateController.getLatestAdmission);
 router.get("/get/school", AdmissionUpdateController.getSchools);
-router.get("/get/ut/university", AdmissionUpdateController.getColleges);
-router.get("/get/college", AdmissionUpdateController.getUniversities);
+router.get("/get/college", AdmissionUpdateController.getColleges);
+router.get("/get/university", AdmissionUpdateController.getUniversities);
 
 module.exports = router;
